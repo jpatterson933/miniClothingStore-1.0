3e98f9fefa39496b697966826166ae663c77d542
Jeffery Patterson <jpatterson933@ucla.edu>
2021-09-07 14:05:26 -0700

    Added our first schema for tshirts and added tshirt dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,16 +11,16 @@ const {
 
 // dummy data
 var tshirts = [
-    { shirtType: 'Polo', size: 'XS', color: 'Blue', upc: 00001, id: 1 },
-    { shirtType: 'Henley', size: 'M', color: 'Black', upc: 00002, id: 2 },
-    { shirtType: 'V-neck', size: 'L', color: 'White', upc: 00003, id: 3 },
-    { shirtType: 'Striped', size: 'XL', color: 'Grey', upc: 00004, id: 4 },
-    { shirtType: 'Graphic', size: 'XXL', color: 'Green', upc: 00005, id: 5 },
-    { shirtType: 'Pocket', size: 'XS', color: 'Red', upc: 00010, id: 6 },
-    { shirtType: 'Solid/Plain', size: 'M', color: 'Orange', upc: 00020, id: 7 },
-    { shirtType: 'Hooded', size: 'L', color: 'Purple', upc: 00030, id: 8 },
-    { shirtType: 'Crew Neck', size: 'XL', color: 'Green', upc: 00040, id: 9 },
-    { shirtType: 'U Neck/Scoop Neck', size: 'XXL', color: 'Blue', upc: 00050, id: 10 },
+    { shirtType: 'Polo', size: 'XS', color: 'Blue', upc: 00001, id: '1' },
+    { shirtType: 'Henley', size: 'M', color: 'Black', upc: 00002, id: '2' },
+    { shirtType: 'V-neck', size: 'L', color: 'White', upc: 00003, id: '3' },
+    { shirtType: 'Striped', size: 'XL', color: 'Grey', upc: 00004, id: '4' },
+    { shirtType: 'Graphic', size: 'XXL', color: 'Green', upc: 00005, id: '5' },
+    { shirtType: 'Pocket', size: 'XS', color: 'Red', upc: 00010, id: '6' },
+    { shirtType: 'Solid/Plain', size: 'M', color: 'Orange', upc: 00020, id: '7' },
+    { shirtType: 'Hooded', size: 'L', color: 'Purple', upc: 00030, id: '8' },
+    { shirtType: 'Crew Neck', size: 'XL', color: 'Green', upc: 00040, id: '9' },
+    { shirtType: 'U Neck/Scoop Neck', size: 'XXL', color: 'Blue', upc: 00050, id: '10' }
 ]
 
 const TshirtType = new GraphQLObjectType({
