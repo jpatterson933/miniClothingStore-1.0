66b41ef5734e1f53f21513434d4afad0549169c5
Jeffery Patterson <jpatterson933@ucla.edu>
2021-09-09 23:20:02 -0700

    Now have the ability to look at colors and sizes for both shirts and pants that exist in the mongo database that we have set up for this mini store application.

--- client/src/components/AddPant.js
+++ client/src/components/AddPant.js
@@ -2,31 +2,47 @@ import React, { Component } from 'react';
 import { gql } from 'apollo-boost';
 // helps us bind apollo to react
 import { graphql } from 'react-apollo';
-import { flowRight as compose } from 'lodash';
+// import { flowRight as compose } from 'lodash';
 
 // FIRST - we construct this query that is below
 // same query structure as graphql //
-const getSizesQuery = gql`
+const getSizeColorQuery = gql`
     {
         sizes{
             id
             name
         }
-    }
-`;
-
-const getColorsQuery = gql`
-    {
         colors{
             id
             name
         }
     }
-
 `;
 
 // SECOND we bind the query we have constructed to our component
 class AddPant extends Component {
+
+    displaySizes() {
+        let data = this.props.data;
+        if (data.loading) {
+            return (<option disabled>Loading Sizes..</option>)
+        } else {
+            return data.sizes.map(size => {
+                return (<option key={size.id} value={size.id}>{size.name}</option>);
+            })
+        }
+    };
+
+    displayColors() {
+        let data = this.props.data;
+        if(data.loading) {
+            return ( <option disabled>Loading Colors...</option>);
+        } else {
+            return data.colors.map(color => {
+                return (<option key={color.id} value={color.id}>{color.name}</option>);
+            })
+        }
+    };
 
     render() {
 
@@ -46,12 +62,14 @@ class AddPant extends Component {
                 <input type="text" placeholder="Add New Color" />
                 <select>
                     <option>Select Color</option>
+                    {this.displayColors()}
                 </select>
             </div>
             <div className="field">
                 <label>Size: </label>
                 <select>
                     <option>Select Size</option>
+                    {this.displaySizes()}
                 </select>
             </div>
             <button>Add Pants</button>
@@ -61,7 +79,4 @@ class AddPant extends Component {
     }
 }
 
-export default compose(
-    graphql(getSizesQuery, {name: "getSizesQuery"}),
-    graphql(getColorsQuery, {name: "getColorsQuery"})
-)(AddPant)
+export default graphql(getSizeColorQuery)(AddPant);
